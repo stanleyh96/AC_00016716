9f66ce79b9b862ebb92204e114fb05b01dd95557
stanleyh96 <00016716@uca.edu.sv>
2021-04-19 11:56:24 -0600

    modified archive

--- labo2/tarealabo2.asm
+++ labo2/tarealabo2.asm
@@ -1,20 +1,23 @@
-        org      100h
+         org      100h
 
         section  .text
 
         mov      byte [200h], "S"
+
         mov      byte [201h], "A"
+
         mov      byte [202h], "H"
+
         mov      byte [203h], "B"
 
         ;directo
         mov      AX, [200h]
         ;indirecto
         mov      BX, [201h]
-        mov      CX,DS[BX]
+        mov      CX,[BX]
         ;INDIRECTO BASE+INDICE
-        mov      BX,[202h]
-        mov      DX,DS[BX+DI]
+        mov      SI,[5h]
+        mov      DX,[BX+SI]
 
         int      20h
 
